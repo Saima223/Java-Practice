9902343a7588ebfdef0c226449531112f4c80865
Saima <siddiqui.22062004@gmail.com>
2024-06-14 01:41:09 +0530

    Formatted the files

--- Hello.java
+++ Hello.java
@@ -3,46 +3,46 @@ public class Hello {
 	public static void main(String[] args) {
 		// 1st code
 		System.out.println("Hello!!!");
-        //dataTypes int, float,char and boolean
-		int num1=9;
-		byte by=127;
-		short sh=558;
-		long l=5854l;
-		
-		float f=5.8f;
-		double d=5.8;
-		
-		char c='k';
-		
-		boolean b=true; //by default value is false 
+		// dataTypes int, float,char and boolean
+		int num1 = 9;
+		byte by = 127;
+		short sh = 558;
+		long l = 5854l;
 
-		//literal
-		int n=0b101;
+		float f = 5.8f;
+		double d = 5.8;
+
+		char c = 'k';
+
+		boolean b = true; // by default value is false
+
+		// literal
+		int n = 0b101;
 		System.out.println(n);
-		
-		int num2=0x7E;
+
+		int num2 = 0x7E;
 		System.out.println(num2);
-		
-		int num3=10_00_00_000;
+
+		int num3 = 10_00_00_000;
 		System.out.println(num3);
-		
-		float num4=56;
+
+		float num4 = 56;
 		System.out.println(num4);
-		
-		double num5=56;
+
+		double num5 = 56;
 		System.out.println(num5);
-		
-		double num6=12e10;
+
+		double num6 = 12e10;
 		System.out.println(num6);
-		
-		boolean num7= true;
+
+		boolean num7 = true;
 		System.out.println(num7);
-		
-		char ch='a';
+
+		char ch = 'a';
 		System.out.println(ch);
 		c++;
-		
-		char c1='a';
+
+		char c1 = 'a';
 		c1++;
 		System.out.println(c1);
 
