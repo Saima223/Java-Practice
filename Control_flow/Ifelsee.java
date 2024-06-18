2e91cacb1b0aaea0a5cd2e695386bdd24f9d5d9b
Saima223 <siddiqui22062004@gmail.com>
2024-06-18 19:41:01 +0530

    started Control flow

---
+++ Control_flow/Ifelsee.java
@@ -0,0 +1,14 @@
+package Control_flow;
+
+public class Ifelsee {
+    public static void main(String[] args) {
+            int number = 10;
+    
+            // Check if the number is even or odd
+            if (number % 2 == 0) {
+                System.out.println("The number is even.");
+            } else {
+                System.out.println("The number is odd.");
+            }
+       }
+}
