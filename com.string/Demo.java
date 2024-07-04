497ae89be3a02d2723fc8a11ff4eab9790dd7424
Saima223 <siddiqui22062004@gmail.com>
2024-07-04 23:32:51 +0530

    String day 1

---
+++ com.string/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        String str1 = "Using String literal";
+
+        String str2 = new String("print using new keyword!");
+
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
